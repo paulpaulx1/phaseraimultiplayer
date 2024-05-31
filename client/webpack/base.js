965cbc82908d23760e573b093281c5afb61e925a
Paul Neenan <paulneenan@Pauls-MBP.lan>
2024-05-30 22:11:32 -0400

    Add entry point to Webpack configuration

--- client/webpack/base.js
+++ client/webpack/base.js
@@ -12,7 +12,6 @@ module.exports = {
     filename: "bundle.js",
     publicPath: "/"
   },
-
   module: {
     rules: [
       {
--- client/webpack/prod.js
+++ client/webpack/prod.js
@@ -8,7 +8,7 @@ module.exports = merge(base, {
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: "bundle.min.js",
-    publicPath: "/"  // Ensure the publicPath is correct
+    publicPath: "/"
   },
   devtool: false,
   performance: {
